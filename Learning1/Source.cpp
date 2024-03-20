b5e7daded5f14738a5b7c10f8e2309f2c7ba94f3
Oleg Rubenchik <oleg.rubenchik25@gmail.com>
2024-03-20 21:41:14 +0200

    Added some new functions and started working on an interface

--- Learning1/Source.cpp
+++ Learning1/Source.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 using namespace std;
 //RANDOM PRACTICE
 // --------------
 //arrays
 void Arrays1d();
 void Arrays2d();
-void dynamic_array();
-void pointer_to_array();
 
 //strings
 void class_string();
 
 //references and pointers
-void pointers();
 void references();
 void references_reassigning();
-void pointers_and_references_in_a_functions();
-void pointers_memory_allocation();
+void pointers();
+void pointers_reassigning();
+void pointer_to_array();
+void pointer_to_pointer();
 
+//functions
+void pointers_and_references_in_a_functions();
+
+//dynamic memory
+ void dynamic_memory_allocation();
+ void dynamic_memory_allocation_arrays();
+
+ //------------
 //SOFDEV
 void Lab3Testing();
 
@@ -26,20 +34,24 @@ void Lab3Testing();
 void w5_EX1();
 void w5_EX2();
 void w5_EX3();
-void w5_EX4();
+void w5_EX4(); 
 void w5_EX5();
+
+// Technical functions
+void wholeInterface();
+int printMenu();
 
 //GLOBAL CONSTANTS
 const int MAX = 2;
 const int MAXLEN = 255; // For strings
 
 int main() {
-	pointer_to_array();
+	wholeInterface();
 
 	return 0;
 }
 
-
+// arrays
 void Arrays1d() {
 	int A[MAX],
 		B[MAX] = { 0 };
@@ -86,37 +98,17 @@ void Arrays2d() {
 		cout << endl;
 	}
 }
-void pointer_to_array() {
-	int ARRAY[] = { 1,3,5 };
-	cout << ARRAY; // the ADDRESS of the FIRST ELEMENT in the ARRAY
-	int* p;
-	p = ARRAY; // copying that ADDRESS to a pointer "p"
-	cout << *(p + 1); // printing the VALUE that STORES in the ADDRESS of (1+1) ELEMENT of an ARRAY
-	
-	
-	cout << p[2] << endl << *p;
-}
-void Lab3Testing() {
-	int total[10];
-	const int SIZE = 10;
-	for (int i = 0; i < SIZE; i++) {
-		total[i] = i;
-	}
-	for (int i = 0; i < SIZE; i++) {
-		cout << total[i] << '\n';
-	}
-}
-void class_string() {
-	string str = " Yo, wassup?";
-	str += '!';
-	cout << str + " Nigga"  << endl;
-}
+
 void dynamic_array() {
-	int size = 7;
+	int size = 7; 
 	int* A = new int[size] {1,3};
 	cout << *A;
 	delete []A;
 }
+
+
+
+// references and pointers
 void references() {
 	int x = 5;
 	int& rX = x;
@@ -147,9 +139,16 @@ void pointers() {
 	*p = 10;
 	cout << "Changes has been made. (*p = 10;)\n";
 	cout << "Now the value (X) is: " << x << endl; // Now X == 10
-	cout << endl << "--------------------------------------------------" << endl;
+}
+void pointers_reassigning() {
 	//YOU CAN CHANGE THE ADDRESS WHERE POINTER IS POINTING TO:
+	int x = 5; //A VALUE
+	int* p = &x; //A POINTER
 	int y = 1;
+	cout << "The value (X): " << x << endl; // the VALUE
+	cout << "The address of a value (&x): " << &x << endl; // the ADDRESS of a VALUE
+	cout << "The pointer (p): " << p << endl; // the ADDRESS where POINTER POINTS
+	cout << "The value that pointer is pointing to (*p): " << *p << endl; // the VALUE thats stores in the ADDRESS where POINTER POINTS
 	cout << "Added a new variable (int y = 1;)\n";
 	cout << "The address where pointer is pointing to: " << p << endl;
 	p = &y; // Reassigning "p" to a NEW ADDRESS
@@ -160,10 +159,68 @@ void pointers() {
 	cout << endl;
 	cout << endl;
 }
-void pointers_memory_allocation(){
-
+void pointer_to_array() {
+	int ARRAY[] = { 1,3,5 };
+	cout << "The ADDRESS of the first elemnt of an ARRAY: " << ARRAY << endl; // the ADDRESS of the FIRST ELEMENT in the ARRAY
+	int* p;
+	p = ARRAY; // copying the ADDRESS of the FIRST ELEMENT of an ARRAY to a pointer "p"
+	cout << "The VALUE that STORES in the ADDRESS of (1+1) ELEMENT of an ARRAY (*(p + 1)): " << *(p + 1); 
+			   // printing the VALUE that STORES in the ADDRESS of (1+1) ELEMENT of an ARRAY
+}	
+void pointer_to_pointer() {
+	char data[] = "It is a string";
+	cout << sizeof(data);
 }
 
+void dynamic_memory_allocation(){ // dynamicly allocate an integer variable
+	// TWO KEYWORDS: new AND delete
+	// new - creates a variable in a HEAP. We can access the variables in a HEAP only by using pointers.
+	// delete - deletes that variable from a HEAP (obviously).
+
+	int* pointer_to_new_variable/**stores the ADDRESS*/ = new int;// returns a memory address 
+
+	// "pointer_to_new_variable" was statically allocated at compile time
+	// unnamed variable(new int) dynamically allocated at run time
+	// so new we have TWO variables. One in the STACK and one in teh HEAP
+
+	*pointer_to_new_variable = 8; //A variable that stores in the HEAP was accessed by using a pointer and got initialized
+	cout << *pointer_to_new_variable;
+	delete pointer_to_new_variable; // Free the memory(delete) of anything that stores in the ADDRESS where this POINTER IS POINTING TO
+} 
+void dynamic_memory_allocation_arrays() {
+	int n; // Statically allocated variable (in the STACK)
+	int* array_pointer; // Also statically allocated variable (in the STACK)
+	
+	//Now let's DYNAMICLLY allocate n integers
+	cout << "Enter the size of an array: ";
+	cin >> n;
+
+	//Creating an array of a size n using dynamic memory allocation
+	array_pointer = new int[n]; // NOTE: we use (int) because we want to store integers
+
+	//Let the user insert all numbers
+	for (int i = 0; i < n; i++) {
+		cout << "Enter a number at poistion " << i << ": ";
+		cin >> array_pointer[i];
+	}
+	//Show an array
+
+	//WE CAN USE TWO METHODS FOR ACCESSING THE ELEMENTS OF DYNAMIC ARRAYS;
+	// First one. Like with a simple array:
+	for (int i = 0; i < n; i++) 
+		cout << array_pointer[i] << ' ';
+	cout << endl;
+	cout << endl;
+	// Second one. Use pointer notaion. 
+	// REMINDER: "array_pointer" stores the ADDRESS of the FIRST ELEMENT of an ARRAY
+	for (int i = 0; i < n; i++)
+		cout << *(array_pointer + i) << ' ';
+	cout << endl;
+
+	//To delete an ENTIRE ARRAY from a HEAP we need to use square brackets before the name.
+	delete []array_pointer; 
+}
+ 
 //Functions for "pointers_and_references"
 	void changeToValue_Nothing(int variable, int value) { //passed by value
 		variable = value;
@@ -187,6 +244,28 @@ void pointers_and_references_in_a_functions() {
 
 	changeToValue_WithPointer(&x, 15); // A function is copying the ADDRESS of x and changes A VALUE STORED IN THAT ADDRESS.
 	cout << x << endl; // Now X = 15
+}
+
+
+
+
+
+void class_string() {
+	string str = " Yo, wassup?";
+	str += '!';
+	cout << str + " Nigga"  << endl;
+}
+
+// sofdev
+void Lab3Testing() {
+	int total[10];
+	const int SIZE = 10;
+	for (int i = 0; i < SIZE; i++) {
+		total[i] = i;
+	}
+	for (int i = 0; i < SIZE; i++) {
+		cout << total[i] << '\n';
+	}
 }
 
 
@@ -309,3 +388,127 @@ void w5_EX5() {
 	delete[]A;
 }
 
+
+int printMenu() {
+	int choice;
+	cout << endl;
+	cout << "0) Quit\n";
+	cout << "1) Practice C++ topics\n";
+	cout << "2) See solutions for PP1 exercises from moodle\n";
+	cout << "Enter your choice: ";
+	cin >> choice;
+	cout << endl;
+	return choice;
+}
+int practiceMenu() {
+	int choice = 0;
+	cout << endl;
+	cout << "0) Quit\n";
+	cout << "1) Arrays1d()\n";
+	cout << "2) Arrays2d()\n";
+	cout << "-----------------\n";
+	cout << "3) class_string()\n";
+	cout << "-----------------\n";
+	cout << "4) references()\n";
+	cout << "5) references_reassigning() \n";
+	cout << "6) pointers()\n";
+	cout << "7) pointers_reassigning()\n";
+	cout << "8) pointer_to_array()\n";
+	cout << "9) pointer_to_pointer()\n";
+	cout << "-----------------\n";
+	cout << "10) pointers_and_references_in_a_functions()\n";
+	cout << "-----------------\n";
+	cout << "11) dynamic_memory_allocation()\n";
+	cout << "12) dynamic_memory_allocation_arrays()\n";
+	cout << "Enter your choice: ";
+	cin >> choice;
+	cout << endl;
+	return choice;
+}
+bool practiceAction(int choice) {
+	switch (choice) {
+	case 0: return false;
+	case 1:
+		system("CLS");
+		Arrays1d();
+		cout << endl;
+		cout << endl;
+		cout << endl;
+		cout << endl;
+		break;
+	case 2:
+		system("CLS");
+		Arrays2d();
+		break;
+	case 3:
+		system("CLS");
+		class_string();
+		break;
+	case 4:
+		system("CLS");
+		references();
+		break;
+	case 5:
+		system("CLS");
+		references_reassigning();
+		break;
+	case 6:
+		system("CLS");
+		pointers();
+		break;
+	case 7:
+		system("CLS");
+		pointers_reassigning();
+		break;
+	case 8:
+		system("CLS");
+		pointer_to_array();
+		break;
+	case 9:
+		system("CLS");
+		pointer_to_pointer();
+		break;
+	case 10:
+		system("CLS");
+		pointers_and_references_in_a_functions();
+		break;
+	case 11:
+		system("CLS");
+		dynamic_memory_allocation();
+		break;
+	case 12:
+		system("CLS");
+		dynamic_memory_allocation_arrays();
+		break;
+	default:
+		system("CLS");
+		cerr << "Are you dumb?" << endl;
+		break;
+
+
+		
+	}
+}
+void wholeInterface() {
+	bool flag_0 = true;
+	while (flag_0) {
+		switch (printMenu()) {
+		case 0:
+			flag_0 = false;
+			int choice;
+			cout << "Enter anythng to leave: ";
+			cin >> choice;
+			break;
+		case 1:
+			while (practiceAction(practiceMenu()));  // "practiceAction" returns false if "practiceMenu" returns 0.
+			break;
+		case 2:
+			cout << "Developing..." << endl;
+			break;
+		default:
+			cerr << "Are you dumb?" << endl;
+			break;
+		}
+	
+	}
+}
